89fbb5d731541fcbf406d01655c4ed9c5dfbb4a3
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-02-20 21:11:08 -0700

    Created file to hold utility functions

---
+++ utilities.py
@@ -0,0 +1,10 @@
+# Contains utility functions for the project.
+
+def nfa2Gtg(gv):
+    """
+    Converts an NFA to an equivalent GTG.
+
+    :param gv: digraph object of the NFA to convert
+    :return: An equivalent GTG to the NFA
+    """
+    print("Write code")
