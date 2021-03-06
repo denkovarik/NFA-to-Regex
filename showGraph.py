63648dce33a4d09da706cabc02957a8593335f44
Dennis <dennis.kovarik@mines.sdsmt.edu>
2021-03-05 19:02:48 -0700

    Added documentation for file

--- showGraph.py
+++ showGraph.py
@@ -1,3 +1,15 @@
+# File: showGraph.py
+# Author: Dennis Kovarik
+# Purpose: Displays an NFA discribed in dot notation
+#
+# Description:
+# This program simply just displays an NFA described in dot notation to the 
+# screen as a pdf. This program expects the file path to the NFA described in
+# dot notation to be passed in on the command line.
+#
+# Usage:
+#       python showGraph.py path/to/graphviv/nfa.gv
+
 import sys
 import graphviz
 import networkx as nx
